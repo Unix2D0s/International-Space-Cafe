06893df730cc1250ed0c384d8b6e29b5652ec513
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-04-16 02:33:19 +1200

    Create Parallax Effect

---
+++ frontend/src/components/ParallaxScroll.jsx
@@ -0,0 +1,11 @@
+import './ParallaxScroll.css'
+
+const ParallaxScroll = () => {
+    return (
+        <div className="hero-container">
+            <div className='hero-bg'></div>
+        </div>
+    )
+}
+
+export default ParallaxScroll
