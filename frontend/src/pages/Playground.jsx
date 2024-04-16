ce28bdeb21e01c19194d70489013ee975b6dbb32
CharlesCryer <charliecac201@gmail.com>
2024-04-16 21:22:05 +1200

    starting carousel

--- frontend/src/pages/Playground.jsx
+++ frontend/src/pages/Playground.jsx
@@ -1,7 +1,18 @@
-export default function Playground(){
-    return (
-        <div>
-            <h3>write your carousel code here</h3>
-        </div>
-    )
+import "../modules/Playground.css";
+import { useState } from "react";
+export default function Playground() {
+  const [currentClass, setCurrentClass] = useState("carousel_container");
+  const transition = () => {
+    if (currentClass == "carousel_container")
+      setCurrentClass("carousel_container transition");
+    else setCurrentClass("carousel_container");
+  };
+  return (
+    <>
+      <div className={currentClass}>
+        <h3>Carousel slide</h3>
+      </div>
+      <button onClick={transition}>Click to start transition</button>
+    </>
+  );
 }
